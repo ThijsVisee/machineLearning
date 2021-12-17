bad6bf0ff4f3dceef599975c49f999bdef1633dc
nielsRocholl <nielsrocholl@gmail.com>
2021-12-17 14:46:13 +0100

    added pitch encoding

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,6 +1,8 @@
+import math
+
 import numpy as np
 import os as os
-
+import pandas as pd
 
 class VoiceData:
     def __init__(self):
@@ -31,21 +33,34 @@ class VoiceData:
         for idx, voice in enumerate(self.raw_data):
             pitch_encoded_voice = []
             for note in voice:
-                log_abs_pitch = self.__get_log_abs_pitch(note)
-                x_chroma, y_chroma = self.__get_x_y_chroma(note)
-                x_fifths, y_fifths = self.__get_x_y_fifths(note)
-                v = [log_abs_pitch, x_chroma, y_chroma, x_fifths, y_fifths]
+                if note == 0:
+                    v = [0, 0, 0, 0, 0]
+                else:
+                    log_abs_pitch = self.__get_log_abs_pitch(note)
+                    x_chroma, y_chroma = self.__get_x_y_chroma(note)
+                    x_fifths, y_fifths = self.__get_x_y_fifths(note)
+                    v = [log_abs_pitch, x_chroma, y_chroma, x_fifths, y_fifths]
                 pitch_encoded_voice.append(v)
             self.encoded_data.append(pitch_encoded_voice)
             print(f"Voice {idx} encoded")
         print("Pitch Encoded Successfully!")
+        # pd.DataFrame(self.encoded_data).to_csv("file.csv")
 
     '''
     return logarithm of the absolute pitch
     '''
 
     def __get_log_abs_pitch(self, note):
-        log_abs_pitch = 0
+        lowest_note = 35
+        highest_note = 74
+
+        n = note - 69
+        fx = math.pow(2, (n / 12)) * 440
+
+        min_p = 2 * math.log2(math.pow(2, ((lowest_note - 69) / 12)) * 440)
+        max_p = 2 * math.log2(math.pow(2, ((highest_note - 69) / 12)) * 440)
+
+        log_abs_pitch = 2 * math.log2(fx) - max_p + (max_p - min_p)/2
         return log_abs_pitch
 
     '''
@@ -53,8 +68,12 @@ class VoiceData:
     '''
 
     def __get_x_y_chroma(self, note):
-        x = 0
-        y = 0
+        chroma = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        radius_chroma = 1
+        note = ((note - 55) % 12)
+        chroma_angle = (chroma[note] - 1) * (360 / 12)
+        x = radius_chroma * math.sin(math.degrees(chroma_angle))
+        y = radius_chroma * math.sin(math.degrees(chroma_angle))
         return x, y
 
     '''
@@ -62,6 +81,13 @@ class VoiceData:
     '''
 
     def __get_x_y_fifths(self, note):
-        x = 0
-        y = 0
+        c5 = [1, 8, 3, 10, 5, 12, 7, 2, 9, 4, 11, 6]
+        radius_c5 = 1
+        note = ((note - 55) % 12)
+
+        c5_angle = (c5[note] - 1) * (360 / 12)
+        x = radius_c5 * math.sin(math.degrees(c5_angle))
+        y = radius_c5 * math.sin(math.degrees(c5_angle))
         return x, y
+
+k = VoiceData()
