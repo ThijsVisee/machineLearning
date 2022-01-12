6df4db2712c596a5821565682a850cd540a99134
Thijs Visee <thijsvisee@gmail.com>
2022-01-12 13:59:38 +0100

    bef pull

--- model/linear_regression.py
+++ model/linear_regression.py
@@ -23,12 +23,7 @@ class LinearRegression:
         """
         Fit the model with ridge regularization.
         """
-        print(X)
-        print(X.T @ X)
         inverse = np.linalg.inv(X.T @ X + self.ridge_alpha * np.identity(X.shape[1]))
-        print(inverse.shape)
-        print(X.shape)
-        print(y.shape)
         # return np.linalg.inv(X.T @ X + np.eye(X.shape[0]) * (self.ridge_alpha ** 2)) @ X.T @ y
         inv_times_XT = inverse @ X.T
         return inv_times_XT @ y
@@ -44,16 +39,14 @@ def main():
     x1 = np.linspace(0, 9, 10)
     x2 = np.linspace(2, 20, 10)
     X  = np.array([x1, x2])
-    y = np.array([4 * idx + idx_2 + 0.25 - 0.5 * random.random() for idx, idx_2 in zip(x1, x2)])
-    model = LinearRegression(X, y, ridge_alpha=0.005)
+    y = np.array([4 * idx + idx_2 + 1 - 2 * random.random() for idx, idx_2 in zip(x1, x2)])
+    model = LinearRegression(X, y, ridge_alpha=0.0001)
     pred = np.array([model.predict(X.T[idx]) for idx in range(10)])
+    print(pred)
     
     plt.plot(y, y)
     plt.plot(y, pred)
     plt.show()
-
-
-
 
 
 if __name__ == "__main__":
