ba707986a4b64b9c70911e53c661ce91d79ecf69
Thijs Visee <thijsvisee@gmail.com>
2022-01-19 16:25:47 +0100

    still on duration

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def flatten_list(l):
 def main(d, voice, preceding_notes):
     duration_data = [d.encoded_data[voice][0].copy()]
     duration_data[0].append(1)
+    max_duration = 0
     for idx, data in enumerate(d.encoded_data[voice][1:]):
-        if len(data) < 4:
-            continue
         if data == duration_data[-1][0:5]:
             duration_data[-1][5] += 1
+            if duration_data[-1][0] != 0:
+                max_duration = max(max_duration, duration_data[-1][5])
         else:
             duration_data.append(data.copy())
             duration_data[-1].append(1)
+    
 
     X = []
     y = []
@@ -25,7 +27,7 @@ def main(d, voice, preceding_notes):
         if idx <= preceding_notes:
             continue
         y.append(np.array([data[0], data[5]]))
-        X.append(flatten_list(d.encoded_data[voice][idx - preceding_notes:idx]))
+        X.append(flatten_list(duration_data[idx - preceding_notes:idx]))
     
     X = np.array(X).T
     y = np.array(y)
@@ -35,7 +37,7 @@ def main(d, voice, preceding_notes):
     idx == len(duration_data) - 1
     print(duration_data[-1])
     while idx < 1300:
-        predicted_pitch, duration = model.predict(flatten_list(d.encoded_data[voice][-preceding_notes-1:-1]))
+        predicted_pitch, duration = model.predict(flatten_list(duration_data[-preceding_notes-1:-1]))
         predicted_pitch = d.get_pitch_from_absolute(predicted_pitch)
         print(f"{idx}: {predicted_pitch}, {duration}")
         if duration > 12:
@@ -46,8 +48,6 @@ def main(d, voice, preceding_notes):
             print(d.encoded_data[voice][-1])
             print(d.encoded_data[voice][-2])
 
-
-    return d.encoded_data[voice].append(predicted_pitch.tolist())
 
 
 if __name__ == '__main__':
