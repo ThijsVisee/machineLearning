00e0db6fa59ba96c8bbb52a5ce6b8fe093552a04
Thijs Visee <thijsvisee@gmail.com>
2022-01-19 15:39:02 +0100

    fairly accurate

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def flatten_list(l):
     return [item for sublist in l for item in sublist]
 
 
-def main(d, voice, prec):
+def main(d, voice, preceding_notes):
     duration_data = [d.encoded_data[voice][0].copy()]
     duration_data[0].append(1)
     for idx, data in enumerate(d.encoded_data[voice][1:]):
@@ -22,28 +22,32 @@ def main(d, voice, prec):
     X = []
     y = []
     for idx, data in enumerate(duration_data):
-        if idx <= prec:
+        if idx <= preceding_notes:
             continue
         y.append(np.array([data[0], data[5]]))
-        print(y[-1])
-        X.append(flatten_list(d.encoded_data[voice][idx - prec:idx]))
+        X.append(flatten_list(d.encoded_data[voice][idx - preceding_notes:idx]))
     
     X = np.array(X).T
     y = np.array(y)
 
     model = LinearRegression(X, y, ridge_alpha=0.005)
 
-    predicted_pitch = model.predict(flatten_list(d.encoded_data[voice][len(d.encoded_data[voice]) - prec:len(d.encoded_data[voice])]))
-    print(d.get_pitch_from_absolute(predicted_pitch[0]))
-    print(predicted_pitch)
+    for idx, data in enumerate(duration_data):
+        if idx <= preceding_notes:
+            continue
+        predicted_pitch = model.predict(flatten_list(d.encoded_data[voice][idx - preceding_notes:idx]))
+        print(idx)
+        print(d.get_pitch_from_absolute(predicted_pitch[0]))
+        print([duration_data[idx][0]] + [duration_data[idx][5]])
+        print(predicted_pitch)
+        print()
 
     return d.encoded_data[voice].append(predicted_pitch.tolist())
 
 
 if __name__ == '__main__':
     VOICE = 1
-    INCLUDED_PRECEDING_STEPS = 12
-    dur = 16
+    INCLUDED_PRECEDING_STEPS = 128
     d = VoiceData()
     # for i in range(dur):
     model = main(d, VOICE, INCLUDED_PRECEDING_STEPS)
